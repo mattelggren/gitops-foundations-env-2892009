a0927d65f2aec8285a06618b94dd4adaa93cefee
Matt Elggren <10578693+mattelggren@users.noreply.github.com>
2026-03-25 14:44:20 -0700

    Updated setup.sh to a) use a different delimiter so forward slashes in sed search don't need to be escaped, and b) excluded the setup.sh and flagger/README.md as files that should not be updated by setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . -type f -exec gsed -i 's/mattelggren/'$1'/g' {} +
+find . -type f -not -name 'setup.sh' -and -not -name 'README.md' -exec gsed -i 's|{dockerHubUsername}|'$1'|g' {} +
